adca05710381418961bed3798e924698899b4bb0
tushargomkar <tushargomkar@gmail.com>
2023-11-10 16:18:10 +0530

    Added new code

---
+++ Pattern2.cpp
@@ -0,0 +1,140 @@
+// 111
+// 222
+// 333
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int n;
+//   cout << "Enter the number" << endl;
+//   cin >> n;
+//   int i = 1;
+
+//   while (i <=n) {
+//     int j=1;
+//     while(j<=n){
+//       cout<<i;
+//       j++;
+//     }
+//     i++;
+//     cout<<endl;
+//   }
+// }
+
+// 123
+// 123
+// 123
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int n;
+//   cout << "Enter the number" << endl;
+//   cin >> n;
+//   int i = 1;
+
+//   while (i <=n) {
+//     int j=1;
+//     while(j<=n){
+//       cout<<j;
+//       j++;
+//     }
+//     i++;
+//     cout<<endl;
+//   }
+// }
+
+// 321
+// 321
+// 321
+
+//   #include <iostream>
+//   using namespace std;
+//   int main() {
+//     int n;
+//     cout << "Enter the number" << endl;
+//     cin >> n;
+//     int i = 1;
+
+//     while (i <=n) {
+//       int j=1;
+//       while(j<=n){
+//         cout<<n-j+1;
+//         j++;
+//       }
+//       i++;
+//       cout<<endl;
+//     }
+//   }
+
+// 123
+// 456
+// 789
+
+//   #include <iostream>
+//   using namespace std;
+//   int main() {
+//     int n;
+//     cout << "Enter the number" << endl;
+//     cin >> n;
+//     int i = 1;
+//     int count=1;
+//     while (i <=n) {
+//       int j=1;
+//       while(j<=n){
+//         cout<<count;
+//         j++;
+//         count++;
+//       }
+//       i++;
+//       cout<<endl;
+//     }
+//   }
+
+// *
+// **
+// ***
+// ****
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int n;
+//   cout << "Enter the number" << endl;
+//   cin >> n;
+//   int i = 1;
+//   while (i <=n) {
+//     int j=1;
+//     while(j<=i){
+//       cout<<"*";
+//       j++;
+
+//     }
+//     i++;
+//     cout<<endl;
+//   }
+// }
+
+// 1
+// 22
+// 333
+// 4444
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int n;
+//   cout << "Enter the number" << endl;
+//   cin >> n;
+//   int i = 1;
+//   while (i <=n) {
+//     int j=1;
+//     while(j<=i){
+//       cout<<i;
+//       j++;
+
+//     }
+//     i++;
+//     cout<<endl;
+//   }
+// }
